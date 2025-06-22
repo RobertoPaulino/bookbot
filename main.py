886442206e452e23a57442ed6331d00127012f5e
Robert <rpaulinodev@gmail.com>
2025-06-22 18:41:00 -0400

    new function to count instances of characters added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_number_of_words
+from stats import get_instances_of_characters
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -7,8 +8,9 @@ def get_book_text(file_path):
     return text
 
 def main():
-    num_of_words = get_number_of_words(get_book_text("books/frankenstein.txt"))
+    book_text = get_book_text("books/frankenstein.txt")
+    num_of_words = get_number_of_words(book_text)
     print(f"{num_of_words} words found in the document")
-
+    print(get_instances_of_characters(book_text))
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def get_number_of_words(book_text):
     list_of_words = book_text.split()
     return len(list_of_words)
+
+def get_instances_of_characters(book_text):
+    lowered_string = book_text.lower()
+    character_dict = {}
+
+    for char in lowered_string:
+        if char not in character_dict:
+            character_dict[char] = 1
+        else:
+            character_dict[char] += 1
+
+    return character_dict
