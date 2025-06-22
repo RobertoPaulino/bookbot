7e3fd2fe171cbbbcad86fb37f9b08623c0ac082e
Robert <rpaulinodev@gmail.com>
2025-06-22 18:34:19 -0400

    refactored stat function into another file

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
+from stats import get_number_of_words
 
 def get_book_text(file_path):
     with open(file_path) as f:
         text = f.read()
 
     return text
-
-def get_number_of_words(book_text):
-    list_of_words = book_text.split()
-    return len(list_of_words)
 
 def main():
     num_of_words = get_number_of_words(get_book_text("books/frankenstein.txt"))
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_number_of_words(book_text):
+    list_of_words = book_text.split()
+    return len(list_of_words)
