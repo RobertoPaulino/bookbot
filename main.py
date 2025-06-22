a4e2fc5a2cdb1a4c148a2f4899a4c2d705c57dd5
Robert <rpaulinodev@gmail.com>
2025-06-22 18:28:55 -0400

    added function to read files

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+def get_book_text(file_path):
+    with open(file_path) as f:
+        text = f.read()
 
+    return text
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+main()
